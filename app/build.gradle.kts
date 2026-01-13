da13dfa31caac858e766da08743a9bbb65f153fa
FakeGamingAbid <fakeabidrahman@gmail.com>
2026-01-13 11:06:33 +0600

    Bump version code to 2 and version name to 2.0.0
    Updated version code and name in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.hiltAndroid)
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fitu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -112,4 +112,4 @@ dependencies {
 
 kapt {
     correctErrorTypes = true
-}
+} 
