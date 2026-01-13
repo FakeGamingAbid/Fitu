0626614053bcc45e92a30ee19b23399e82f141b8
FakeGamingAbid <fakeabidrahman@gmail.com>
2026-01-13 11:41:36 +0600

    Add StepEntity and StepDao to FituDatabase

--- app/src/main/java/com/fitu/data/local/FituDatabase.kt
+++ app/src/main/java/com/fitu/data/local/FituDatabase.kt
@@ -3,19 +3,31 @@ package com.fitu.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.fitu.data.local.dao.MealDao
+import com.fitu.data.local.dao.StepDao
 import com.fitu.data.local.dao.WorkoutDao
 import com.fitu.data.local.dao.WorkoutPlanDao
+import com.fitu.data.local.dao.FoodCacheDao
 import com.fitu.data.local.entity.MealEntity
+import com.fitu.data.local.entity.StepEntity
 import com.fitu.data.local.entity.WorkoutEntity
 import com.fitu.data.local.entity.WorkoutPlanEntity
-
-import com.fitu.data.local.dao.FoodCacheDao
 import com.fitu.data.local.entity.FoodCacheEntity
 
-@Database(entities = [WorkoutEntity::class, MealEntity::class, WorkoutPlanEntity::class, FoodCacheEntity::class], version = 4)
+@Database(
+    entities = [
+        WorkoutEntity::class,
+        MealEntity::class,
+        WorkoutPlanEntity::class,
+        FoodCacheEntity::class,
+        StepEntity::class
+    ],
+    version = 5,
+    exportSchema = false
+)
 abstract class FituDatabase : RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
     abstract fun mealDao(): MealDao
     abstract fun workoutPlanDao(): WorkoutPlanDao
     abstract fun foodCacheDao(): FoodCacheDao
-}
+    abstract fun stepDao(): StepDao
+} 
