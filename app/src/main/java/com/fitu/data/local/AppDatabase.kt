247d4ea59899b28d145b97f4406449217165a78c
FakeGamingAbid <fakeabidrahman@gmail.com>
2026-01-17 18:34:44 +0600

    Update AppDatabase.kt

--- app/src/main/java/com/fitu/data/local/AppDatabase.kt
+++ app/src/main/java/com/fitu/data/local/AppDatabase.kt
@@ -3,20 +3,26 @@ package com.fitu.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.fitu.data.local.dao.FoodCacheDao
 import com.fitu.data.local.dao.MealDao
 import com.fitu.data.local.dao.StepDao
 import com.fitu.data.local.dao.WorkoutDao
+import com.fitu.data.local.dao.WorkoutPlanDao
+import com.fitu.data.local.entity.FoodCacheEntity
 import com.fitu.data.local.entity.MealEntity
 import com.fitu.data.local.entity.StepEntity
 import com.fitu.data.local.entity.WorkoutEntity
+import com.fitu.data.local.entity.WorkoutPlanEntity
 
 @Database(
     entities = [
         StepEntity::class,
         MealEntity::class,
-        WorkoutEntity::class
+        WorkoutEntity::class,
+        WorkoutPlanEntity::class,
+        FoodCacheEntity::class
     ],
-    version = 8,
+    version = 9,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -24,4 +30,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun stepDao(): StepDao
     abstract fun mealDao(): MealDao
     abstract fun workoutDao(): WorkoutDao
+    abstract fun workoutPlanDao(): WorkoutPlanDao
+    abstract fun foodCacheDao(): FoodCacheDao
 }
