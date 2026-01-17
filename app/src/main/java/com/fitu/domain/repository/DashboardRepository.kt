dbba2c3f99abf2f8d12b900fa0d9bf2d21930e17
FakeGamingAbid <fakeabidrahman@gmail.com>
2026-01-17 17:30:45 +0600

    Update DashboardRepository.kt

--- app/src/main/java/com/fitu/domain/repository/DashboardRepository.kt
+++ app/src/main/java/com/fitu/domain/repository/DashboardRepository.kt
@@ -7,4 +7,12 @@ import kotlinx.coroutines.flow.Flow
 interface DashboardRepository {
     fun getMealsForDay(startOfDay: Long, endOfDay: Long): Flow<List<MealEntity>>
     fun getWorkoutsForDay(startOfDay: Long, endOfDay: Long): Flow<List<WorkoutEntity>>
+    fun getCaloriesConsumedForDay(startOfDay: Long, endOfDay: Long): Flow<Int?>
+    fun getCaloriesBurnedForDay(startOfDay: Long, endOfDay: Long): Flow<Int?>
+    fun getProteinForDay(startOfDay: Long, endOfDay: Long): Flow<Float?>
+    fun getCarbsForDay(startOfDay: Long, endOfDay: Long): Flow<Float?>
+    fun getFatsForDay(startOfDay: Long, endOfDay: Long): Flow<Float?>
+    suspend fun insertMeal(meal: MealEntity): Long
+    suspend fun deleteMeal(id: Long)
+    suspend fun insertWorkout(workout: WorkoutEntity): Long
 }
